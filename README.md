feb7685c2fd9dc96ee84fe875503827409aa07b1
Computer Maverick <43058711+ComputerMaverick69@users.noreply.github.com>
2019-08-30 06:18:07 -0700

    upgraded to run on Python 3

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # <img src="https://ee5817f8e2e9a2e34042-3365e7f0719651e5b8d0979bce83c558.ssl.cf5.rackcdn.com/python.png" width="48"><img src="https://sucuri.net/images/icons/features/large/malware-detection-website-icon-large.png" width="48">url-scanner
-This small program coded with python and it use virustotal api v2 to scan urls and get report.
+
+This program was originally developed using python 2.x. It makes use of  VirusTotal api v2 to scan urls and get report.
 
 
 **Program Requirements:**
 
-- python2.7 [Download link](https://www.python.org/download/releases/2.7/)
-- virustotal api [how to get virustotal api?](https://www.virustotal.com/en/documentation/public-api/)
+- Python 3.x [Download link](https://www.python.org/)
+- VirusTotal api [How to obtain API?](https://www.virustotal.com/en/documentation/public-api/)
 
 
 **How to use?**
 
 Windows:
 
-1. [Download](https://github.com/magic-coding/url-scanner/archive/master.zip) & Extract url-scanner.zip file.
+1. [Download](https://github.com/ComputerMaverick69/web-application-security-scanner/archive/master.zip) & Extract the zipped files.
 2. Edit `scan.py` file using any code editor.
-3. Add your virustotal api:
+3. Update the Api_key to your virustotal api key:
 ```
 Api_Key = "xxxxxxxxxxxxxxxxxxxxxxx"
 ```
@@ -25,14 +26,14 @@ Linux:
 
 Open new terminal and type this command:
 
-1- install `Git` & if you install `git` aleardy please skip this step.
+1- Install `Git` & if `git` is aleardy installed, please skip this step.
 ```
 apt-get install git
 ```
-2- install url-scanner program and run:
+2- Install web-application-security-scanner program and run:
 ```
-git https://github.com/magic-coding/url-scanner.git
-cp url-scanner/scan.py ~
+git https://github.com/ComputerMaverick69/web-application-security-scanner.git
+cp web-application-security-scanner/scan.py ~
 ```
 3- Edit `scan.py` and add your virustotal api:
 ```
@@ -53,13 +54,8 @@ python scan.py
 
 **Have a problem?**
 
-Please open new issue or contact with me via Twitter [@magic_coding](http://www.twitter.com/magic_coding)
-
-
-**Conclusion**
-
-In Conclusion hope you enjoy with this small programe and don't forget to follow me to get more free open source programes.
+Please open new issue or contact with me via Twitter [@codemaverick69](http://www.twitter.com/codemaverick69)
 
 **License:**
 
-As of March 22, 2016 url-scanner is licensed under the GPLv3+: http://choosealicense.com/licenses/gpl-3.0
+As of August 29, 2019 web-application-security-scanner is licensed under the GPLv3+: http://choosealicense.com/licenses/gpl-3.0
--- scan.py
+++ scan.py
@@ -4,8 +4,8 @@
 #  Twitter: @magic_coding
 
 import json
-import urllib
-import urllib2
+from urllib.request import Request, urlopen
+import urllib.parse
 import time
 import simplejson
 import webbrowser
@@ -16,7 +16,7 @@ import sys
 VirusTotal Public API:
 Learn more how to get VirusTotal API (https://www.virustotal.com/en/documentation/public-api/)
 """
-Api_Key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+Api_Key = "cf51132ff1e33e6501428d45b94ba594cd2bd86504984163925e18aa2ce03938"
 
 def progressbar(it, prefix="", size=60):
 	count = len(it)
@@ -33,12 +33,13 @@ def progressbar(it, prefix="", size=60):
 	sys.stdout.flush()
 
 def scanurl():
-	get_link = raw_input("Link to scan: ")
+	get_link = input("Link to scan: ")
 	url = "https://www.virustotal.com/vtapi/v2/url/report"
 	parameters = {"resource": get_link,"apikey": Api_Key}
-	data = urllib.urlencode(parameters)
-	req = urllib2.Request(url, data)
-	response = urllib2.urlopen(req)
+	data = urllib.parse.urlencode(parameters)
+	data = data.encode('utf-8')
+	req = Request(url, data)
+	response = urlopen(req)
 	json = response.read()
 	response_dict = simplejson.loads(json)
 	scan_id = response_dict.get("scan_id")
@@ -48,29 +49,29 @@ def scanurl():
 	analysis = response_dict.get("permalink")
 	Positives = response_dict.get("Positives")
 	total = response_dict.get("total")
-	for i in progressbar(range(15), "Scanning: ", 40):
+	for i in progressbar(range(100), "Scanning: ", 50):
 		time.sleep(0.7)
 	if response_code==1:
 		cls()
-		print """   ______                __   _       __           __   __
+		print (r"""   ______                __   _       __           __   __
   / ____/___  ____  ____/ /  | |     / /___  _____/ /__/ /
  / / __/ __ \/ __ \/ __  /   | | /| / / __ \/ ___/ //_/ / 
 / /_/ / /_/ / /_/ / /_/ /    | |/ |/ / /_/ / /  / ,< /_/  
-\____/\____/\____/\__,_/     |__/|__/\____/_/  /_/|_(_)\n"""
-		print "Scan finished, Check your scan information bellow:\n"
-		# print "|Scan ID: "+scan_id
-		print "|Link: "+link
-		print "|Scan Date: "+scan_date
-		print "|Scan report url: "+analysis
-		print "|Scanner: "+str(total)+ " Scanner."
-		print "|Positives: "+str(Positives)
-		again = raw_input("\nOpen report link in browser? (y/n): ")
-		if again=="y":
+\____/\____/\____/\__,_/     |__/|__/\____/_/  /_/|_(_)\n""")
+		print ("Scan finished, Check your scan information bellow:\n")
+		print ("|Scan ID: "+scan_id)
+		print ("|Link: "+link)
+		print ("|Scan Date: "+scan_date)
+		print ("|Scan report url: "+analysis)
+		print ("|Scanner: "+str(total)+ " Scanner.")
+		print ("|Positives: "+str(Positives))
+		again = input("\nOpen report link in browser? (Y/N): ")
+		if again == "y" or again == "Y":
 			webbrowser.open(analysis)
 		else:
 			pass
 	else:
-		enter = raw_input("[!] There is error. Please try again..[Press Enter]")
+		enter = input("[!] There is error. Please try again..[Press Enter]")
 
 def cls():
 	os.system('cls' if os.name=='nt' else 'clear')
@@ -78,40 +79,34 @@ def cls():
 
 def info():
 	cls()
-	print """   __  ______  __       _____                                 
+	print (r"""   __  ______  __       _____                                 
   / / / / __ \/ /      / ___/_________ _____  ____  ___  _____
  / / / / /_/ / /       \__ \/ ___/ __ `/ __ \/ __ \/ _ \/ ___/
 / /_/ / _, _/ /___    ___/ / /__/ /_/ / / / / / / /  __/ /    
-\____/_/ |_/_____/   /____/\___/\__,_/_/ /_/_/ /_/\___/_/\n"""
-	print """This program coded by Mahmoud Al-nafei (www.twitter.com/magic_coding)
-with Python language and it's free for use.
-You can get this programe as open source from my Github account:
-www.github.com/magic-coding
-
-Enjoy..
------
-For Help: @magic_coding
-	"""
-	Enter = raw_input("press (Enter) to go back..")
+\____/_/ |_/_____/   /____/\___/\__,_/_/ /_/_/ /_/\___/_/\n""")
+	print ("""This program was originally developed by Mahmoud Al-nafei (www.twitter.com/magic_coding) and upgraded to run Python 3 by Timmy Iwoni (twitter.com/codemaverick6).
+It's free for use.""")
+	Enter = input("press (Enter) to go back..")
 	if Enter:
 		pass
 
 while True:
 	cls()
-	print """   __  ______  __       _____                                 
+	print (r"""   __  ______  __       _____                                 
   / / / / __ \/ /      / ___/_________ _____  ____  ___  _____
  / / / / /_/ / /       \__ \/ ___/ __ `/ __ \/ __ \/ _ \/ ___/
 / /_/ / _, _/ /___    ___/ / /__/ /_/ / / / / / / /  __/ /    
 \____/_/ |_/_____/   /____/\___/\__,_/_/ /_/_/ /_/\___/_/     
                                                               
-Scan any url using more than 60 scanner from (virustotal).
-Credit @magic_coding (www.twitter.com/magic_coding)\n
--------------------------\n"""
-	print "1- Scan url with (VirusTotal)"
-	print "2- about"
-	print "3- Exit System"
+Scan any website using more than 60 scanner from (virustotal).
+Originally developed by @magic_coding (www.twitter.com/magic_coding)\n
+Upgraded by @codemaverick69 (twitter.com/codemaverick69)\n
+-------------------------\n""")
+	print ("1- Scan Website")
+	print ("2- About Developer")
+	print ("3- Exit System")
 	try:
-		decision = raw_input("Please select a number: ")
+		decision = input("Select your action: ")
 		if decision.isdigit() == True:
 			if(int(decision) == 1):
 				scanurl()
@@ -119,21 +114,21 @@ Credit @magic_coding (www.twitter.com/magic_coding)\n
 				info()
 			elif(int(decision) == 3):
 				cls()
-				print """  ________                __                       
+				print (r"""  ________                __                       
  /_  __/ /_  ____ _____  / /__   __  ______  __  __
   / / / __ \/ __ `/ __ \/ //_/  / / / / __ \/ / / /
  / / / / / / /_/ / / / / ,<    / /_/ / /_/ / /_/ / 
 /_/ /_/ /_/\__,_/_/ /_/_/|_|   \__, /\____/\__,_/  
-                              /____/\n"""
-				print "program exit.."
-				print "Thank you for using URL Scanner by @magic_coding"
+                              /____/\n""")
+				print ("Program Closing.....")
+				print ("Thank you for using URL Scanner, originally developed by @magic_coding, Upgraded to Python 3 by @codemaverick69")
 				exit()
 			else:
-				print "Error"
+				print ("Error")
 		else:
-			print "[!] Please enter number not string."
+			print ("[!] Please enter number not string.")
 			time.sleep(2)
 	except KeyboardInterrupt:
-		print "\nprogram exit.."
-		print "See you again."
+		print ("\nProgram Interrupted..")
+		print ("See you again.")
 		exit()
